e42883f929e99b51b4d2e5123785350e487155de
Shaambhavi Priya <154751877+Shaam-p@users.noreply.github.com>
2026-01-28 18:40:39 +0530

    Add a score limit.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const buttons = document.querySelectorAll(".buttons button");
 
 const scoreA = document.getElementById("scoreA");
 const scoreB = document.getElementById("scoreB");
+const max_score = 21;
 
 let countA = 0;
 let countB = 0;
@@ -14,14 +15,17 @@ let countB = 0;
 buttons.forEach((button) => {
   button.addEventListener("click", function () {
     // console.log(button.id);
-    if (button.id === "A") {
+    if (button.id === "A" && countA < max_score) {
       countA += 1;
       scoreA.value = countA;
     }
 
-    if (button.id === "B") {
+    if (button.id === "B" && countB < max_score) {
       countB += 1;
       scoreB.value = countB;
+    }
+    if (countA === MAX_SCORE || countB === MAX_SCORE){
+      console.log("Game Over")
     }
   });
 });
