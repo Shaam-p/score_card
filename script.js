3d70ce876ce017040d1244084f0ce91f2017a70d
Shaambhavi Priya <154751877+Shaam-p@users.noreply.github.com>
2026-01-28 18:26:47 +0530

    add to the function to update live_score with buttons.

--- script.js
+++ script.js
@@ -5,8 +5,23 @@
 // }
 const buttons = document.querySelectorAll(".buttons button");
 
+const scoreA = document.getElementById("scoreA");
+const scoreB = document.getElementById("scoreB");
+
+let countA = 0;
+let countB = 0;
+
 buttons.forEach((button) => {
   button.addEventListener("click", function () {
-    console.log(button.id);
+    // console.log(button.id);
+    if (button.id === "A") {
+      countA += 1;
+      scoreA.value = countA;
+    }
+
+    if (button.id === "B") {
+      countB += 1;
+      scoreB.value = countB;
+    }
   });
 });
